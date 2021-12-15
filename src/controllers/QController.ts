ea0005ad943ed3b72162cbb62a2717f6a9e89d41
Darin Kelkhoff <darin.kelkhoff@kingsrook.com>
2021-12-14 18:32:13 -0600

    Adding copyright

--- src/controllers/QController.ts
+++ src/controllers/QController.ts
@@ -1,3 +1,7 @@
+/*
+ * Copyright © 2021-2021. Kingsrook LLC <contact@kingsrook.com>.  All Rights Reserved.
+ */
+
 // export default (a: number, b: number): number => a + b;
 
 import {QTableMetaData} from '../model/metaData/QTableMetaData';
