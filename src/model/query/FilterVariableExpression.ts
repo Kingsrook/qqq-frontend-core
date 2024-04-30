41182928a5af3b427a1db947cc1ce4d0280dd43c
Tim Chamberlain <tchamberlain76@gmail.com>
2024-04-30 10:00:15 -0500

    CE-1179: added filter variable expression

---
+++ src/model/query/FilterVariableExpression.ts
@@ -0,0 +1,44 @@
+/*
+ * QQQ - Low-code Application Framework for Engineers.
+ * Copyright (C) 2021-2022.  Kingsrook, LLC
+ * 651 N Broad St Ste 205 # 6917 | Middletown DE 19709 | United States
+ * contact@kingsrook.com
+ * https://github.com/Kingsrook/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+/*******************************************************************************
+ ** Define a "filter variable" type expression, as part of a criteria in a QQQ instance.
+ **
+ *******************************************************************************/
+export class FilterVariableExpression
+{
+   fieldName?: string;
+   valueIndex?: number;
+   type: "FilterVariableExpression";
+
+   constructor(object?: any)
+   {
+      this.fieldName = object?.fieldName;
+      this.valueIndex = object?.valueIndex;
+      this.type = "FilterVariableExpression";
+   }
+
+   toString()
+   {
+      return "${VARIABLE}";
+   }
+}
